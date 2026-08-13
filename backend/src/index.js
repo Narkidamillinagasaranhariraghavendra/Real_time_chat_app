38d6b995c94d28cda5bcb2d84b4b2692c17c4628
sushma-23s <sushmanarkidamill@gmail.com>
2026-08-13 13:45:36 +0530

    changes happened

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,18 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import fs from "fs";
+import path from "path";
 import clerkmiddleware from "@clerk/express"
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 const frontendUrl = process.env.frontend_url;
-
+const publicDir = path.join(process.cwd(),"public");
 app.use(express.json());
 app.use(cors({ origin: frontendUrl, credentials: true }));
 app.use(clerkMiddleware());
@@ -17,6 +20,15 @@ app.get("/health",(req,res)=>{
     
     res.status(200).json({ message: "Server is running!" });
 });
+
+if (fs.existsSync(publicDir)){
+
+    app.use(express.static(publicDir));
+
+    app.get("/{*any}",  (req,res,next)=>{
+        res.sendFile(path.join(publicDir, "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     connectDB();
