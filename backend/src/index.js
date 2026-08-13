8eff6072843cb47c885b847153b73c17357bdb93
Narkidamilli Hari Raghavendra <narkidamillihariraghavendra@gmail.com>
2026-08-13 18:20:51 +0530

    client web hooks

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,8 @@ import path from "path";
 import { clerkMiddleware } from "@clerk/express";
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
+import clerkWebhook from "./webhooks/clerk.webhook.js";
+import job from "./lib/cron.js";
 
 
 
@@ -13,6 +15,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const frontendUrl = process.env.frontend_url;
 const publicDir = path.join(process.cwd(),"public");
+//it should be in raw format because clerk sends the webhook in raw format and if we use express.json() then it will not work
+app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }),clerkWebhook);
 app.use(express.json());
 app.use(cors({ origin: frontendUrl, credentials: true }));
 app.use(clerkMiddleware());
